b3edc9b35ce01cf324e03b802444f6015bf14a6a
Daniel Ebonine <ebonined@gmail.com>
2022-09-09 22:37:45 +0100

    variables if else while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,15 +15,14 @@ int main(void)
 {
 	int n;
 	char str[100];
-        char lastchar[1];
-        int lastint;
+    char lastchar[100];
+    int lastint;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	sprintf(str, "%d", n);
-	lastchar[0] = str[1];
-	printf("%s", lastchar);
+	lastchar[0] = str[strlen(str)-1];
 	lastint = atoi(lastchar);
 
 	if (n < 0)
