5fa24a93e9c369f144b5ba0ffebbb83f331af357
Daniel Ebonine <ebonined@gmail.com>
2022-09-10 16:17:17 +0100

    variables if else while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num1;
+	int num2;
+	int sum = 0;
+	int index = 0;
+
+	for (num1 = '0'; num1 <= '9'; num1++)
+	{
+		for (num2 = '1'; num2 <= '9'; num2++)
+		{
+			if ((num1 * num2) > sum)
+			{
+				putchar(num1);
+				putchar(num2);
+				if (num1 < '9' || num2 < '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				sum = num1 * num2;
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
