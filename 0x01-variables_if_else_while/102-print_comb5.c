9f6a7739c8d4d5453634b7513d27c87d43a46132
Daniel Ebonine <ebonined@gmail.com>
2022-09-11 02:26:12 +0100

    variables if else while task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,6 +8,8 @@
 
 int main(void)
 {
+	int pnum1 = -1;
+	int pnum2;
 	int num1;
 	int num2;
 	int num3;
@@ -15,28 +17,29 @@ int main(void)
 
 	for (num1 = '0'; num1 <= '9'; num1++)
 	{
-		for (num2 = '0'; num2 <= '8'; num2++)
+		for (num2 = '0'; num2 <= '9'; num2++)
 		{
+			++pnum1;
+			pnum2 = -1;
 			for (num3 = '0'; num3 <= '9'; num3++)
 			{
 				for (num4 = '0'; num4 <= '9'; num4++)
 				{
-					if ((num1 != num3 || num2 != num4) &&
-						(num1 <= num3 && num2 <= num4))
+					++pnum2;
+					if (pnum1 < pnum2)
 					{
 						putchar(num1);
-	                                        putchar(num2);
-        	                                putchar(' ');
-                	                        putchar(num3);
-                        	                putchar(num4);
-						
-						if (num1 != '9' || num2 != '8' || num3 != '9' || num4 != '9')
+						putchar(num2);
+						putchar(' ');
+						putchar(num3);
+						putchar(num4);
+
+						if (pnum1 != 98 || pnum2 != 99)
 						{
 							putchar(',');
 							putchar(' ');
 						}
 					}
-
 				}
 			}
 		}
