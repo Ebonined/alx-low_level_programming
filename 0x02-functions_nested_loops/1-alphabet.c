7df80e921aca09ad16d9556b4e95217573f0acf5
Daniel Ebonine <ebonined@gmail.com>
2022-09-13 22:39:59 +0100

    functions nested loops task 1 cor 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,22 @@
 /**
  * main - Entry point
  *
+ * print_alphabet - another variable
+ *
+ * _putchar - print char
+ * @str: variable
+ *
  * Return: Always (0) Success
  *
  */
 
-void print_alphabet(void);
+void _putchar(char str);
 
 int main(void)
 {
 	char alpha;
 
-	for (alpha = 'a', alpha <= 'z'; alpha++)
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		_putchar(alpha);
 	}
