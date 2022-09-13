e22f4068d81b68f15b70bbc538a810daac30c0ce
Daniel Ebonine <ebonined@gmail.com>
2022-09-13 21:13:27 +0100

    functions nested loops task 0 cor 4

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,20 @@
 /**
  * main - Entry point
  *
+ * _putchar - printing function
+ * @str: character to be printed
+ *
  * Return: Always (0) Success
  */
 
-void _putchar();
+/* function declaration */
+void _putchar(char str[]);
 
 int main(void)
 {
-	_putchar();
+	char str[] = "_putchar";
+
+	_putchar(str);
 
 	return (0);
 }
