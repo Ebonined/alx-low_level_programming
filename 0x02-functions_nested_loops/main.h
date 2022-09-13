7c09a8bfdfaca2423e81e08f079f701fcf03960d
Daniel Ebonine <ebonined@gmail.com>
2022-09-14 00:17:09 +0100

    functions nested loops task 4 main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -63,3 +63,26 @@ int _isalpha(int c)
 	return num;
 }
 
+int print_sign(int n)
+{
+	int num;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -1;
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		num = 1;
+	}
+	else
+	{
+		_putchar("0");
+		num = 0;
+	}
+
+	return num;
+}
+
