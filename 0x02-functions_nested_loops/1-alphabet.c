9372bf1a1700cf7fe95ee2ceab90c47aa00fed72
Daniel Ebonine <ebonined@gmail.com>
2022-09-13 23:11:32 +0100

    functions nested loops task 1 cor 9

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,6 @@
 /**
  * main - Entry point
  *
- * print_alphabet - another variable
- *
- * _putchar - print char
- * @str: variable
- *
  * Return: Always (0) Success
  *
  */
-
-void print_alphabet(void);
-
