eef4621fa69eec77a6b5e1c856786c0ff3956e85
Daniel Ebonine <ebonined@gmail.com>
2022-09-14 21:03:52 +0100

    to pc

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) Success
+ */
+
+int main(void)
+{
+        long int intarr[100];
+        long int tempnum;
+	long int sum = 0;
+	long int value;
+        int index = 0;
+	int lasti = 0;
+
+        intarr[0] = 1;
+        intarr[1] = 2;
+
+	while (1)
+	{
+		tempnum = intarr[index] + intarr[index + 1];
+
+
+		if (tempnum < 4000000)
+		{
+			intarr[index + 2] = tempnum;
+			lasti = index;
+		}
+		else
+		{
+			break;
+		}
+
+		index++;
+	}
+
+	printf("%d\n", lasti);
+
+	for (index = 0; index < sizeof(intarr); index++)
+	{
+		value = intarr[index];
+		if (value % 2 == 0)
+		{
+			sum = sum + value;
+		}
+	}
+
+	printf("%ld\n", sum);
+
+        return (0);
+}
+
