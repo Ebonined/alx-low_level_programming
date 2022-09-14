81523c6b5e190c14634d520bd73d379a8b1ee47e
Daniel Ebonine <ebonined@gmail.com>
2022-09-14 19:50:39 +0100

    functions nested loops task 13

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) Success
+ */
+
+int main(void)
+{
+	long int intarr[50];
+	int index;
+
+	intarr[0] = 1;
+	intarr[1] = 2;
+	long int tempnum;
+	
+	printf("%ld\n", intarr[0]);
+	printf("%ld\n", intarr[1]);
+
+	for (index = 0; index <= 47; index++)
+	{
+		tempnum = intarr[index] + intarr[index + 1];
+		intarr[index + 2] = tempnum;
+		printf("%ld\n", tempnum);
+	}
+
+	return (0);
+}
