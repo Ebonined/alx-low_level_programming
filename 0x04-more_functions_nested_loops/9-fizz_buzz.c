38e454e43ce409bda1a2c6f1cab8ece111bee9be
Daniel Ebonine <ebonined@gmail.com>
2022-09-16 04:05:59 +0100

    functions nested loops task 9 c1

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 5) == 0 && num == 100)
+                {
+                        printf("buzz");
+                }
+		else if ((num % 5) != 0 && (num % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((num % 5) == 0 && (num % 3) != 0)
+		{
+			printf("buzz ");
+		}	
+		else if ((num % 5) == 0 && (num % 3) == 0)
+                {
+                        printf("Fizzbuzz ");
+                }
+		else if ((num == 1))
+		{
+			printf("%d ", num);
+		}
+		else if ((num == 100))
+                {
+                        printf("%d", num);
+                }
+		else
+		{
+			printf(" %d ", num);
+		}
+	}
+
+	printf("\n");
+
+	return (0);
+}
