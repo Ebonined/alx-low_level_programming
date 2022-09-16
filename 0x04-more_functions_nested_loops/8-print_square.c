47fceb02550ce18a7f7e7bfa63f0f9e32863e208
Daniel Ebonine <ebonined@gmail.com>
2022-09-16 03:13:25 +0100

    functions nested loops task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,34 @@
+/**
+ * _putchar - print char
+ * @str: string to be converted
+ */
+void _putchar(char str);
+
+/**
+ * print_square - print square
+ * @size: size of square
+ */
+void print_square(int size)
+{
+	int l = size;
+	int w = size;
+	int numw;
+	int numl;
+
+	if (size > 0)
+	{
+
+		for (numl = 0; numl < l; numl++)
+		{
+			for (numw = 0; numw < w; numw++)
+			{
+				_putchar('#');
+				if (numw == (w - 1))
+				{
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
+
