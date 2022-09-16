5161950f5af2fbcb7449b15be9b61ed15d9af67a
Daniel Ebonine <ebonined@gmail.com>
2022-09-16 01:38:25 +0100

    functions nested loops task 5

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -64,3 +64,44 @@ void print_most_numbers(void)
 		}
 	}
 }
+
+/**
+ * print_int - print integer function
+ * @value: value in int
+ */
+
+void print_int(int value)
+{
+	if (value != 0)
+	{
+		print_int(value / 10);
+		_putchar((value % 10) + '0');
+	}
+}
+/**
+ * more_numbers - head function
+ */
+void more_numbers(void)
+{
+	char num = 4;
+	int line;
+
+	for (line = 0; line < 10; line++)
+	{
+		_putchar('0');
+
+		for (num = 0; num <= 14; num++)
+		{
+			if (num < 14)
+			{
+				print_int(num);
+			}
+
+			else
+			{
+				print_int(num);
+				_putchar('\n');
+			}
+		}
+	}
+}
