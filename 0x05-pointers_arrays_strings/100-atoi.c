bef247d81f61e8ed1bfb424ae14a149d48f391c7
Daniel Ebonine <ebonined@gmail.com>
2022-09-20 02:56:28 +0100

    check

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,1 +1,20 @@
-int _atoi(char *s);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _atoi - str to int function
+ * @s: str variable
+ *
+ * Return: always int
+ */
+
+int _atoi(char *s)
+{
+        int conv;
+   
+
+        conv = atoi(s);
+
+        return (conv);
+}
