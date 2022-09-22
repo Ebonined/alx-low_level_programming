b1530cd7a3d0c50f4a6b813e846a7ac9f380864c
Daniel Ebonine <ebonined@gmail.com>
2022-09-22 05:02:34 +0100

    pointers arrays strings task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,34 +10,11 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int bool = 0;
-	int output;
 
-	while (*(s1 + i))
+	while (*(s1 + i) && (*(s1 + i) == *(s2 + i)))
 	{
-		if (*(s1 + i) == *(s2 + i))
-		{
-			bool += 1;
-		}
-
 		i++;
 	}
 
-	if (bool < i)
-	{
-		if (*(s1 + 0) > *(s2 + 0))
-		{
-			output = 15;
-		}
-		else
-		{
-			output = -15;
-		}
-	}
-	else if (bool == i)
-	{
-		output = 0;
-	}
-
-	return (output);
+	return ((*(s1 + i) -  *(s2 + i)));
 }
