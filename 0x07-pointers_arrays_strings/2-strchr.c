d5ada2db986dc263d42661043aba307887aacd49
Daniel Ebonine <ebonined@gmail.com>
2022-09-26 20:20:03 +0100

    pointers arrays strings 02

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,36 @@
+#include <stddef.h>
+
+/**
+ * _strchr - find char in string
+ * @s: string to be search
+ * @c: char to find
+ *
+ * Return: char*
+ */
+
+char *_strchr(char *s, char c)
+{
+	char *o;
+	int i = 0;
+	int bool = 0;
+
+	while (*(s + i))
+	{
+		if (*(s + i) == c)
+		{
+			bool = 1;
+			break;
+		}
+		i++;
+	}
+
+	if (bool == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		o = &(*(s + i));
+		return (o);
+	}
+}
