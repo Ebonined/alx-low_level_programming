55124bb8d39bdac32c70cba1972d6671706dc710
Daniel Ebonine <ebonined@gmail.com>
2022-09-26 22:00:47 +0100

    pointer arrays strings 03

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,22 +11,25 @@ unsigned int _strspn(char *s, char *accept)
 	int i, j, found;
 	unsigned int count;
 
-	i = j = 0;
+	i = count = 0;
 	while (*(s + i))
 	{
-		found = 0;
+		found = j = 0;
 		while (*(accept + j))
 		{
 			if (*(s + i) == *(accept + j))
 			{
 				count += 1;
 				found = 1;
-			}
-			if (found == 0)
-			{
 				break;
 			}
+			j++;
 		}
+		if (found == 0)
+		{
+			break;
+		}
+		i++;
 	}
 
 	return (count);
