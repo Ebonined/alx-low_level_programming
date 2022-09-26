ba769e21eccb7ffec3dacfd8aaecac13476d68fd
Daniel Ebonine <ebonined@gmail.com>
2022-09-26 22:42:32 +0100

    pointer arrays strings 04

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,31 +1,29 @@
 /**
- * _strpbrk
+ * _strpbrk - function to break with byte in accept is found
  * @s: string to search
  * @accept: acceptable char values
- * 
+ *
  * Return: char *
  */
 
 char *_strpbrk(char *s, char *accept)
 {
-    int i, j;
+	int i, j;
 
-    i = 0;
-    while (*(accept + i))
-    {
-        j = 0;
-        while (*(s + j))
-        {
-           if (*(s + i) == *(accept + j))
-           {
-                return (s + i);
-                break;
-           }
-           j++;
-        }
-        i++;
-    }
+	i = 0;
+	while (*(accept + i))
+	{
+		j = 0;
+		while (*(s + j))
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				return (s + i);
+			}
+			j++;
+		}
+		i++;
+	}
 
-    return (0);
-    
+	return (0);
 }
