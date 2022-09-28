08009d878481a0089f54e1244cb639f3c18e11ce
Daniel Ebonine <ebonined@gmail.com>
2022-09-28 22:17:25 +0100

    recursion 01

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,28 @@
+/**
+ * _putchar - print char
+ * @c: char to print
+ *
+ * Return: int
+ */
+int _putchar(char c);
+
+/**
+ * _print_rev_recursion - print string reverse recursively
+ * @s: string to print
+ *
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+	{
+		_putchar('\n');
+		return;
+	}
+}
