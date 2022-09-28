b508801ceb597ad2f3728eeac9ee7b23d20651cd
Daniel Ebonine <ebonined@gmail.com>
2022-09-28 23:37:32 +0100

    recursion 04

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+/**
+ * _pow_recursion - find power of x raised to y
+ * @x: index
+ * @y: power to be raised to
+ *
+ * Return: int
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, (y - 1)));
+	}
+}
