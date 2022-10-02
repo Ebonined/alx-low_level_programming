8a438ba763921734ea4a8023d4d4f432ab65e514
Daniel Ebonine <ebonined@gmail.com>
2022-10-02 23:15:46 +0100

    argc and argv 01

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function for argc and argv
+ * @argc: arguement count
+ * @argv: argurment variables arrays
+ *
+ * Return: int
+ */
+
+int main(int argc, char *argv[] __attribute__ ((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
