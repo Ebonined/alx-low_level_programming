ef9704a3814d959f885eb879602cc8f850e6ac2e
Daniel Ebonine <ebonined@gmail.com>
2022-10-02 23:09:57 +0100

    argc and argv 00 corr 3

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,16 +5,16 @@
  * main - main function for argc and argv
  * @argc: arguement count
  * @argv: argurment variables arrays
- * 
+ *
  * Return: int
  */
 
 int main(int argc, char *argv[])
 {
-    while (argc--)
-    {
-        printf("%s\n", *argv++);
-    }
+	while (argc--)
+	{
+		printf("%s\n", *argv++);
+	}
 
-    return (0);
+	return (0);
 }
