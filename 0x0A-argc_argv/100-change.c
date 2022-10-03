4f634b38a1fc58d86fe0a17f7916ab1e8ec197db
Daniel Ebonine <ebonined@gmail.com>
2022-10-03 14:44:06 +0100

    argc and argv 05

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints min no. of change for an ammount.
+ * @argc: number of arguments
+ * @argv: argument vector of pointers
+ *
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	int amnt, nchange = 0, ind, cloop;
+	int change[5] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		puts("Error");
+		return (1);
+	}
+	amnt = atoi(argv[1]);
+	if (amnt <= 0)
+	{
+		puts("0");
+		return (1);
+	}
+	else
+	{
+		for (ind = 0; ind < 5; ind++)
+		{
+			cloop = amnt / change[ind];
+			amnt -= cloop * change[ind];
+			nchange += cloop;
+			if (amnt == 0)
+				break;
+		}
+	}
+	printf("%d\n", nchange);
+	return (0);
+}
