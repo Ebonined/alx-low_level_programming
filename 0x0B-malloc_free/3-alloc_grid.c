667a61e618bd0ecc1fc7e3fbc69f1b6ca2fee72d
Daniel Ebonine <ebonined@gmail.com>
2022-10-05 00:20:53 +0100

    malloc free 03

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * alloc_grid - create and allocate grid
+ * @width: width of matrix
+ * @height: height of matrix
+ *
+ * Return: int **
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **out;
+	int m, n, i;
+
+	i = m = n = 0;
+
+	if (width == 0 || height == 0)
+		return (NULL);
+
+	out = malloc(sizeof(**out) * height);
+
+	if (out == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		out[i] = malloc(sizeof(*out) * width);
+	}
+
+	while (m < height)
+	{
+		n = 0;
+		while (n < width)
+		{
+			out[m][n] = 0;
+			n++;
+		}
+		m++;
+	}
+
+	return (out);
+
+}
