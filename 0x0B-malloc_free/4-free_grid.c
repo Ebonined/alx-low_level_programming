1eaf1484fba816f7fac17fb725f247d6c41a1593
Daniel Ebonine <ebonined@gmail.com>
2022-10-05 00:47:25 +0100

    malloc free 04

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - frees previously created grid
+ * @grid: grid to free
+ * @height: height of grid
+ *
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int n;
+
+	if (grid == NULL || height <= 0)
+		return (NULL);
+	for (n = 0; n < height; n++)
+		free(grid[n]);
+	free(grid);
+}
