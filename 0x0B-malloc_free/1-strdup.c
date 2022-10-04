ebd8066c7996db03608fd2f3976024a17f3557d7
Daniel Ebonine <ebonined@gmail.com>
2022-10-04 22:21:55 +0100

    malloc free 01 corr 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,27 +5,39 @@
 /**
  * _strdup - makes copy of string
  * @str: takes in string
- * 
+ *
  * Return: char *
  */
 
 char *_strdup(char *str)
 {
-    int i = 0;
-    char *out;
+	int i = 0;
+	char *out;
 
-    if (str == NULL)
-    {
-        return (NULL);
-    }
-    
-    out = malloc(sizeof(str));
+	while (*(str + i))
+	{
+		i++;
+	}
 
-    while (*(str + i))
-    {
-        out[i] = str[i];
-        i++;
-    }
-    
-    return (out);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	out = malloc(sizeof(*str) * i);
+
+	if (out == NULL)
+	{
+		return (NULL);
+	}
+
+	i = 0;
+	while (*(str + i))
+	{
+		out[i] = str[i];
+		i++;
+	}
+
+	return (out);
 }
