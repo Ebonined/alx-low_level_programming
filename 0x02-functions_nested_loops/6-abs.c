05b6e18c4a3245fd290faa51baa2434aa7986279
Daniel Ebonine <ebonined@gmail.com>
2022-10-04 20:48:28 +0100

    malloc free 00

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,19 @@
-int _abs(int c);
+/**
+ * _abs - outputs absolut value of integer input
+ * @n: integer to check
+ *
+ * Return: n
+ */
+
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+
+	else
+	{
+		n *= -1;
+		return (n);
+	}
+}
