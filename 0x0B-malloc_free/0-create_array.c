aeb495278d806bb00e91dba0bcd91dbd5f2c6b54
Daniel Ebonine <ebonined@gmail.com>
2022-10-04 21:28:18 +0100

    malloc free 00 corr 4

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,12 +15,17 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 	char *out;
 
-	out =  malloc(sizeof(char) * size);
-
 	if (size == 0)
 	{
 		return (NULL);
 	}
+	out =  malloc(sizeof(c) * size);
+
+	if (out == NULL)
+	{
+		return (NULL);
+	}
+	
 	for (i = 0; i < size; i++)
 	{
 		out[i] = c;
