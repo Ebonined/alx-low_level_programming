5156634f51ee7b9cbcdbfdc594520170d3963fbe
Daniel Ebonine <ebonined@gmail.com>
2022-10-08 02:08:13 +0100

    more malloc free 101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -27,7 +27,7 @@ void _print(char *str, int l)
 
 /**
  * mul - multiplies a char with a string and places the answer into dest
- * @n: char to multiply
+ * @ni: char to multiply
  * @num: string to multiply
  * @num_index: last non NULL index of num
  * @dest: destination of multiplication
@@ -35,14 +35,14 @@ void _print(char *str, int l)
  *
  * Return: pointer to dest, or NULL on failure
  */
-char *mul(char n, char *num, int num_index, char *dest, int dest_index)
+char *mul(char ni, char *num, int num_index, char *dest, int dest_index)
 {
 	int j, k, mul, mulrem, add, addrem;
 
 	mulrem = addrem = 0;
 	for (j = num_index, k = dest_index; j >= 0; j--, k--)
 	{
-		mul = (n - '0') * (num[j] - '0') + mulrem;
+		mul = (ni - '0') * (num[j] - '0') + mulrem;
 		mulrem = mul / 10;
 		add = (dest[k] - '0') + (mul % 10) + addrem;
 		addrem = add / 10;
