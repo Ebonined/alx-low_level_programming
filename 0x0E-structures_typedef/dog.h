8d7dc1171c181100d533ab492fed0fed0062ae67
Daniel Ebonine <ebonined@gmail.com>
2022-10-10 12:21:51 +0100

    corr 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owener of dog
+ */
 struct dog
 {
-    char *name;
-    float age;
-    char* owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+#endif
