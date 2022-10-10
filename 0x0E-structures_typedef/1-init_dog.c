74126c6f7365f675c4459dbfc3d53b601481d552
Daniel Ebonine <ebonined@gmail.com>
2022-10-10 11:29:38 +0100

    structure typedef 01

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - function to initialize dog structure
+ * @d: point to dog structure
+ * @name: name parameter for dog structure
+ * @age: age parameter for dog structure
+ * @owner: owner parameter for dog structure
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
