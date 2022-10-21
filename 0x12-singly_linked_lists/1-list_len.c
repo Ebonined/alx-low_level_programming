2262df0fc48d2f1e1e224fd27f8fce031db16056
Daniel Ebonine <ebonined@gmail.com>
2022-10-21 04:46:01 +0100

    singly list 01

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - get the length of h.
+ * @h: link pointer to list
+ * Return: total bodes.
+ */
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+	const list_t *temp;
+
+	temp = h;
+
+	while (temp))
+	{
+		count++;
+		temp = temp->next;
+	}
+	return (count);
+}
