fd34b8113b23cd5bd55ed2003c56dce751f3de7d
Daniel Ebonine <ebonined@gmail.com>
2022-11-05 00:19:53 +0100

    file io task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,11 +18,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-	{
-		return (-1);
-	}
-	else
+	if (text_content != NULL)
 	{
 		while (*(text_content + size))
 		{
